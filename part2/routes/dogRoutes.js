89671bc454673fb0649c37088f452bcfd0212941
AMSOWKAT <a1945730@adelaide.edu.au>
2025-06-20 14:32:43 +0000

    [automatic commit] 2025-06-20 14:32:42 UTC 60.242.229.136

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,4 +5,7 @@ const db = require('../models/db');
 // Get list of dog_id, name for the owner
 
 router.get('/', async (req, res) => {
-    
+    const ownerId = req.session.user?.user_id;
+    if (!ownerId) return res.status(401).json({ error: 'Not logged in' });
+
+    try
