61ad02b001af300289cf3f45dab1f223550d4e7c
AMSOWKAT <a1945730@adelaide.edu.au>
2025-06-20 14:43:40 +0000

    [automatic commit] 2025-06-20 14:43:39 UTC 60.242.229.136

--- part2/app.js
+++ part2/app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const do
+const dogRoutes = require()
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
