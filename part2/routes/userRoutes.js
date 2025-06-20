27c5688f9294de6b922c8ec149e43c2e19805828
AMSOWKAT <a1945730@adelaide.edu.au>
2025-06-20 13:06:11 +0000

    [automatic commit] 2025-06-20 13:06:11 UTC 203.63.93.122

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,7 +61,8 @@ router.post('/login', async (req, res) => {
 router.post('/logout',(req, res) =>{
   req.session.destroy(err =>{
     if (err) return res.status(500).json({ error: 'Logout failed' });
-    res.chunkedEncoding('connect.sid')
+    res.clearCookie('connect.sid'); // clear cookie
+    res
   }
 
   )
