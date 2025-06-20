a140dc8d18556098329897a087a4ab59f0cffb1d
AMSOWKAT <a1945730@adelaide.edu.au>
2025-06-20 14:36:08 +0000

    [automatic commit] 2025-06-20 14:36:08 UTC 60.242.229.136

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -17,5 +17,4 @@ router.get('/', async (req, res) => {
         );
         res.json(dogs);
         } catch (err);
-        res.status(500).json({ error: })
-    }
+        res.status(500).json({ error: 'Failed to fetch dogs'});    }
